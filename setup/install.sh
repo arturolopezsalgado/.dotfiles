77e6d8038ee5c0ec806d87dce2573a8bebbeb4b6
Arturo Lopez Salgado <lopezsalgado.a@gmail.com>
2025-05-02 17:35:59 -0600

    Add install.sh: prerequisites

--- setup/install.sh
+++ setup/install.sh
@@ -2,9 +2,22 @@
 # This script:
 # - Installs
 
-set -e
+# Exit on error, undefined variables, and propagate errors in pipelines
+set -euo pipefail
+
+# Get the absolute path of the directory where the script is located
+readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+# Source installation scripts
+. "$SCRIPT_DIR/utils/colors.sh"
+. "$SCRIPT_DIR/scripts/prerequisites.sh"
 
 echo "Setting up the Mac..."
+echo "Running prerequisites script..."
 
+# Run prerequisites
+detect_architecture
+install_xcode
+install_homebrew
 
 echo "Setup complete"
