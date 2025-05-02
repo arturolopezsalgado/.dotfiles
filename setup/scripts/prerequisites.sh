bac406bb2ddf486d9313573393180a494dcd2e0d
Arturo Lopez Salgado <lopezsalgado.a@gmail.com>
2025-05-01 18:35:44 -0600

    Remove "readonly" in variable

--- setup/scripts/prerequisites.sh
+++ setup/scripts/prerequisites.sh
@@ -4,7 +4,7 @@
 set -euo pipefail
 
 # Get the absolute path of the directory where the script is located
-readonly SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 echo $SCRIPT_DIR
 # Source our colors utility script
