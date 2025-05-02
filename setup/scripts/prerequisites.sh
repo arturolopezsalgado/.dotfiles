7b76c148cef5217bc22dfc1c3f3b560c36688666
Arturo Lopez Salgado <lopezsalgado.a@gmail.com>
2025-05-01 18:38:15 -0600

    Remove unnecesary echo

--- setup/scripts/prerequisites.sh
+++ setup/scripts/prerequisites.sh
@@ -6,7 +6,6 @@ set -euo pipefail
 # Get the absolute path of the directory where the script is located
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo $SCRIPT_DIR
 # Source our colors utility script
 # shellcheck source=./colors.sh
 . "$SCRIPT_DIR/../utils/colors.sh"
